122f514f2d987e220e5aa0ccfddc9e3f9b266332
ChuLiying <oborotsukiyi@gmail.com>
2016-11-10 13:25:28 +0800

    UPDATE CSS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
     $('body').imagesLoaded(function(){
-
-    
-
+        var $window = $(window),
+            _winHeight = $(window).height(),
+            _winWidth = $window.width(),
+            _winR = _winHeight / _winWidth;
+        
+        
+        
     })
-    
-    
- 
-
 })
