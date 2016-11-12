226dd6896828f05ffe53a081a44b1e0df477f2c5
ChuLiying <oborotsukiyi@gmail.com>
2016-11-13 02:58:55 +0800

    TEST

--- js/main.js
+++ js/main.js
@@ -34,26 +34,26 @@ $(document).ready(function(){
         }
         
         // end
-        
-        if(_winWidth>776){
-            // 首頁 about animate
-            $window.scroll(function(){
-
-                var now = $window.scrollTop();
-
-                if( now > _winHeight*0.6 ){
-                    $('.index-about-container').addClass('index-about-animate');
-                    $('.fixed-nav').fadeIn(500);
-                }
-
-                else{
-                    $('.index-about-container').removeClass('index-about-animate');
-                    $('.fixed-nav').fadeOut(500);
-                }
-
-            });
-        }
-        
+//        
+//        if(_winWidth>776){
+//            // 首頁 about animate
+//            $window.scroll(function(){
+//
+//                var now = $window.scrollTop();
+//
+//                if( now > _winHeight*0.6 ){
+//                    $('.index-about-container').addClass('index-about-animate');
+//                    $('.fixed-nav').fadeIn(500);
+//                }
+//
+//                else{
+//                    $('.index-about-container').removeClass('index-about-animate');
+//                    $('.fixed-nav').fadeOut(500);
+//                }
+//
+//            });
+//        }
+//        
         $('.scroll-btn').click(function(){
             $body.animate({
                 scrollTop: _winHeight - 68
