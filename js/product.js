33fe601b93ab6f9c8e990c855636370373342d09
ChuLiying <oborotsukiyi@gmail.com>
2016-11-18 03:56:32 +0800

    UPDATE JS

--- js/product.js
+++ js/product.js
@@ -26,63 +26,141 @@ $(document).ready(function(){
             
             var $sliderDotLi = $('.slider-dot li');
             
+            $imgbox.on("swiperight",function(){
+
+                if( swiping == false ){
+                    return;
+                }
+                
+                var thisKey = $(this).attr("key"),
+                    nowLeft = $(this).css("left"), 
+                    nowLeftInt = parseInt(nowLeft),
+                    newLeft = nowLeftInt +_WH;
+                
+                if (thisKey==0){
+                    return;
+                }
+                
+                else{
+                    swiping = false;
+                    thisKey--;
+                    $imgbox.css("left",newLeft);
+                    $imgbox.attr("key",thisKey);
+                    $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
+                    setTimeout(turnOn,700);
+                }
+            });
+            
             function turnOn(){
                 swiping = true;
             }
             
-            $imgbox.touchwipe({
-                wipeLeft: function() { 
+            $imgbox.on("swipeleft",function(){
+                
+                if( swiping == false ){
+                    return;
+                }
+                
+                var thisKey = $(this).attr("key"),
+                    nowLeft = $(this).css("left"), 
+                    nowLeftInt = parseInt(nowLeft),
+                    newLeft = nowLeftInt -_WH;
+                
+                
+                if (thisKey==_length-1){
+                    return;
+                }
 
-                    if( swiping == false ){
-                        return;
-                    }
-
-                    var thisKey = $(this).attr("key"),
-                        nowLeft = $(this).css("left"), 
-                        nowLeftInt = parseInt(nowLeft),
-                        newLeft = nowLeftInt -_WH;
-
-
-                    if (thisKey==_length-1){
-                        return;
-                    }
-
-                    else{
-                        swiping = false;
-                        thisKey++;
-                        $imgbox.css("left",newLeft);
-                        $imgbox.attr("key",thisKey);
-                        $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
-                        setTimeout(turnOn,700);
-                    }
-                },
-                wipeRight: function() {
-                    if( swiping == false ){
-                        return;
-                    }
-
-                    var thisKey = $(this).attr("key"),
-                        nowLeft = $(this).css("left"), 
-                        nowLeftInt = parseInt(nowLeft),
-                        newLeft = nowLeftInt +_WH;
-
-                    if (thisKey==0){
-                        return;
-                    }
-
-                    else{
-                        swiping = false;
-                        thisKey--;
-                        $imgbox.css("left",newLeft);
-                        $imgbox.attr("key",thisKey);
-                        $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
-                        setTimeout(turnOn,700);
-                    }
-                },
-                min_move_x: 5,
-                min_move_y: 5,
-                preventDefaultEvents: true
+                else{
+                    swiping = false;
+                    thisKey++;
+                    $imgbox.css("left",newLeft);
+                    $imgbox.attr("key",thisKey);
+                    $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
+                    setTimeout(turnOn,700);
+                }
             });
+            
+            var xDown = null;                                                        
+            var yDown = null;    
+            
+            $imgbox.on('touchstart',function(evt){
+                xDown = evt.touches[0].clientX;                                      
+                yDown = evt.touches[0].clientY;      
+            }, false);        
+            
+            $imgbox.on('touchmove', function(evt){
+                if ( ! xDown || ! yDown ) {
+                    return;
+                }
+
+                var xUp = evt.touches[0].clientX;                                    
+                var yUp = evt.touches[0].clientY;
+
+                var xDiff = xDown - xUp;
+                var yDiff = yDown - yUp;
+
+                if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                    if ( xDiff > 0 ) {
+                        /* left swipe */
+                        if( swiping == false ){
+                            return;
+                        }
+
+                        var thisKey = $(this).attr("key"),
+                            nowLeft = $(this).css("left"), 
+                            nowLeftInt = parseInt(nowLeft),
+                            newLeft = nowLeftInt -_WH;
+
+
+                        if (thisKey==_length-1){
+                            return;
+                        }
+
+                        else{
+                            swiping = false;
+                            thisKey++;
+                            $imgbox.css("left",newLeft);
+                            $imgbox.attr("key",thisKey);
+                            $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
+                            setTimeout(turnOn,700);
+                        }
+                    } else {
+                        /* right swipe */
+                        if( swiping == false ){
+                            return;
+                        }
+
+                        var thisKey = $(this).attr("key"),
+                            nowLeft = $(this).css("left"), 
+                            nowLeftInt = parseInt(nowLeft),
+                            newLeft = nowLeftInt +_WH;
+
+                        if (thisKey==0){
+                            return;
+                        }
+
+                        else{
+                            swiping = false;
+                            thisKey--;
+                            $imgbox.css("left",newLeft);
+                            $imgbox.attr("key",thisKey);
+                            $sliderDotLi.removeClass("active").eq(thisKey).addClass("active");
+                            setTimeout(turnOn,700);
+                        }
+                    }                       
+                } else {
+                    if ( yDiff > 0 ) {
+                        /* up swipe */ 
+                    } else { 
+                        /* down swipe */
+                    }                                                                 
+                }
+                /* reset values */
+                xDown = null;
+                yDown = null; 
+            }, false);
+
             
         }
     })
